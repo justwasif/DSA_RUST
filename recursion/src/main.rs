5e55d257350ab07615b54cd18f1da72261e4a7b7
wasif <wasif.iit.ansari@gmail.com>
2026-06-21 13:25:41 +0530

    Add new recursive functions for array manipulation and string processing

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -2,7 +2,8 @@
 #![allow(unused_variables)] // local variables not used
 #![allow(unused_imports)] // imports not used
 
-use std::array;
+use std::str::Chars;
+use std::{array, vec};
 use std::fmt::Alignment::Right;
 use std::fs;
 use std::iter::Product;
@@ -27,13 +28,13 @@ fn recursion_sum(n:i32)->i32{
 
     
 }
-fn fib(n: i32) -> i32 {
-    if n <= 1 {
-        return n;
-    }
+// fn fib(n: i32) -> i32 {
+//     if n <= 1 {
+//         return n;
+//     }
 
-    fib(n - 1) + fib(n - 2)
-}
+//     fib(n - 1) + fib(n - 2)
+// }
 
 fn print_name(count:i32,counter:i32){
     if counter>count{
@@ -135,6 +136,56 @@ fn palindrome(chars: &[char], i: usize) -> bool {
 
     palindrome(chars, i+1)
 }
+fn even_swap(arr: &mut[i32],left:usize,right:usize){
+    if left>=right{
+        return;
+    }
+    arr.swap(left,right);
+    even_swap(arr, left+2, right-2);
+}
+
+fn one_var_swap(arr: &mut[i32],left:usize){
+    let n=arr.len();
+    if left>=n/2{
+        return;
+    }
+    arr.swap(left,n-left-1);
+    one_var_swap(arr, left+1);
+}
+fn string_recursion(word:String,left:usize){
+    let mut chars:Vec<char>=word.chars().collect();
+    let n=chars.len();
+    if left>=n/2{
+        let  s:String=chars.iter().collect();
+        print!("{:?}",s);
+        return;
+    }
+    chars.swap(left, n-left-1);
+    let mut s:String=chars.iter().collect();
+    string_recursion(s, left+1);
+
+
+    
+    
+}
+
+ pub fn fib(n: i32) -> i32 {
+        fn F(i:i32,m:i32)->i32{
+            let mut k=0
+            F(0)=0;
+            F(1)=1;
+            if k>i{
+                k=k+1;
+                m=F(i-1)+F(i-2);
+            }
+            if k==i{
+                return m;
+            }
+            
+            
+        }
+        F
+    }
 
 fn main(){
 //    let number=recursion_sum(10);
@@ -156,9 +207,12 @@ fn main(){
     // print!("{:?}",a);
     // let ar=[0,1,2,3,4,5,6,7,8,9];
     // println!("{:?}",a);
-    let mut ar=[0,1,2,3,4,5,6,7,8,9];
-    let mut n=ar.len();
-    let a=arr_swap(&mut ar, 0,n-1 );
-    println!("{:?}",ar);
+    // let mut ar=[0,1,2,3,4,5,6,7,8,9];
+    // let mut n=ar.len();
+    // let a=even_swap(&mut ar, 0,n-1 );
+    // println!("{:?}",ar);
+    // let a:String="123456".to_string();
+    // let b=string_recursion(a, 0);
+    // print!("{:?}",b);
 
 }
